f31f9a2c795128e97947aee7d9f5166dd67d5117
Matthiee <beerens.matthias@hotmail.com>
2016-03-04 18:46:00 +0100

    Initial commit

---
+++ Flamy2D/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Flamy2D
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+
+
+        }
+    }
+}
