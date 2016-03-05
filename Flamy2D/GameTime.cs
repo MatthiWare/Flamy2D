a96a0df19658c73466b9feb75b9bf12e802e5f25
Matthiee <beerens.matthias@hotmail.com>
2016-03-05 12:22:06 +0100

    Added GameTime class.

---
+++ Flamy2D/GameTime.cs
@@ -0,0 +1,36 @@
+﻿
+
+using System;
+
+namespace Flamy2D
+{
+    /// <summary>
+    /// GameTime contains the total time the game is running and the time since the last update (delta time).
+    /// </summary>
+    public class GameTime
+    {
+
+        /// <summary>
+        /// The total time the game is running for. 
+        /// <see cref="System.TimeSpan"/>
+        /// </summary>
+        public TimeSpan Total { get; private set; }
+
+        /// <summary>
+        /// The delta time (time since last update). 
+        /// <see cref="System.TimeSpan"/>
+        /// </summary>
+        public TimeSpan Elapsed { get; private set; }
+
+
+        /// <summary>
+        /// .ctor for <see cref="Flamy2D.GameTime"/>
+        /// </summary>
+        public GameTime()
+        {
+            Total = new TimeSpan();
+            Elapsed = new TimeSpan();
+        }
+
+    }
+}
