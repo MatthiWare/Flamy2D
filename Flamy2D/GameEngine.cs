44a3538f00f76a7d926c1f48280ee5446e628416
Matthiee <beerens.matthias@hotmail.com>
2016-03-05 15:23:08 +0100

    Made a basic game loop.

--- Flamy2D/GameEngine.cs
+++ Flamy2D/GameEngine.cs
@@ -3,6 +3,8 @@ using Flamy2D.Extensions;
 using OpenTK;
 using OpenTK.Graphics;
 using Flamy2D.Input;
+using System.Threading;
+using OpenTK.Graphics.OpenGL4;
 
 namespace Flamy2D
 {
@@ -22,14 +24,21 @@ namespace Flamy2D
         /// </summary>
         public Resolution Resolution { get; set; }
 
+        /// <summary>
+        /// The Keyboard. 
+        /// </summary>
         public Keyboard Keyboard { get; set; }
 
+        public bool Closing { get; private set; }
 
         private NativeWindow window;
+        private GraphicsContext context;
+        private GraphicsMode graphicsMode;
 
         public GameEngine(GameConfiguration config)
         {
             Configuration = config;
+            Closing = false;
         }
 
         /// <summary>
@@ -38,6 +47,57 @@ namespace Flamy2D
         public void Run()
         {
             Initialize();
+
+            Thread gameloopThread = new Thread(GameLoop);
+            gameloopThread.Name = "Game Loop Thread";
+            gameloopThread.Start();
+        }
+
+        private void GameLoop()
+        {
+            this.Log("Create graphics context");
+
+            context = new GraphicsContext(
+                    mode: graphicsMode,
+                    window: window.WindowInfo,
+                    major: 4,
+                    minor: 0,
+                    flags: GraphicsContextFlags.ForwardCompatible
+                );
+
+            context.MakeCurrent(window.WindowInfo);
+
+            GraphicsContext.Assert();
+
+            this.Log("Enable VSync: {0}", Configuration.VSync);
+            if (Configuration.VSync)
+                context.SwapInterval = 1;
+            else
+                context.SwapInterval = 0;
+
+            this.Log("Loading OpenGL entry points");
+            context.LoadAll();
+
+            this.Log("Setup OpenGL");
+            SetupOpenGL();
+
+            window.Visible = true;
+
+            this.Log("Enter game loop");
+            while (!Closing)
+            {
+
+
+
+
+            }
+
+        }
+
+        protected virtual void SetupOpenGL()
+        {
+            GL.Enable(EnableCap.Blend);
+            GL.BlendFunc(BlendingFactorSrc.SrcAlpha, BlendingFactorDest.OneMinusSrcAlpha);
         }
 
         /// <summary>
@@ -45,6 +105,8 @@ namespace Flamy2D
         /// </summary>
         private void Initialize()
         {
+            graphicsMode = GraphicsMode.Default;
+
             GameWindowFlags flags = GameWindowFlags.Default;
 
             if (Configuration.Fullscreen && !flags.HasFlag(GameWindowFlags.Fullscreen))
@@ -59,7 +121,7 @@ namespace Flamy2D
                 height: Configuration.Height,
                 title: Configuration.Title,
                 options: flags,
-                mode: GraphicsMode.Default,
+                mode: graphicsMode,
                 device: DisplayDevice.Default
             );
 
@@ -67,10 +129,28 @@ namespace Flamy2D
 
             updateResolution();
 
+            // adding window resize event handler
             window.Resize += (o, e) => OnResize();
 
+            // adding keyboard event handler
             window.KeyDown += (o, e) => Keyboard.RegisterKeyDown(o, e);
             window.KeyUp += (o, e) => Keyboard.RegisterKeyUp(o, e);
+
+            window.Closing += (o, e) => {
+                this.Log("Window is closing");
+                Exit();
+
+                // else the window will instantly close and not wait for the game to properly exit. 
+                e.Cancel = true; 
+            };
+
+        }
+
+        public void Exit()
+        {
+            OnExit();
+
+            Closing = true;
         }
 
         /// <summary>
@@ -94,10 +174,12 @@ namespace Flamy2D
         protected virtual void OnResize()
         {
             updateResolution();
-
-
         }
 
+        protected virtual void OnExit()
+        {
+
+        }
 
     }
 }
