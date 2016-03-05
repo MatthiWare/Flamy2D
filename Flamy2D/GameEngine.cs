0225eea674294bdae92ce1cdf1488b879f0a86bf
Matthiee <beerens.matthias@hotmail.com>
2016-03-05 12:22:17 +0100

    Added GameEngine class.

---
+++ Flamy2D/GameEngine.cs
@@ -0,0 +1,33 @@
+﻿using OpenTK;
+using System;
+
+namespace Flamy2D
+{
+    /// <summary>
+    /// The GameEngine containing all the game logic.
+    /// </summary>
+    public class GameEngine
+    {
+        /// <summary>
+        /// The game configuration. 
+        /// </summary>
+        public GameConfiguration Configuration { get; private set; }
+
+        private NativeWindow window;
+
+        public GameEngine(GameConfiguration config)
+        {
+            Configuration = config;
+        }
+
+        /// <summary>
+        /// Runs the game. 
+        /// </summary>
+        public void Run()
+        {
+
+        }
+
+
+    }
+}
