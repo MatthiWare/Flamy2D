3cd8a41f09c94377c0f2c443ea2b9c2eb6fc7d1f
Matthiee <beerens.matthias@hotmail.com>
2016-03-05 13:57:31 +0100

    Setting up NativeWindow

--- Flamy2D/GameEngine.cs
+++ Flamy2D/GameEngine.cs
@@ -10,13 +10,20 @@ namespace Flamy2D
     /// </summary>
     public class GameEngine : ILog
     {
+
         /// <summary>
         /// The game configuration. 
         /// </summary>
         public GameConfiguration Configuration { get; private set; }
 
+        /// <summary>
+        /// The resolution of the game. 
+        /// </summary>
+        public Resolution Resolution { get; set; }
+
+
+
         private NativeWindow window;
-        private GraphicsMode graphicsMode;
 
         public GameEngine(GameConfiguration config)
         {
@@ -36,8 +43,6 @@ namespace Flamy2D
         /// </summary>
         private void Initialize()
         {
-            graphicsMode = GraphicsMode.Default;
-
             GameWindowFlags flags = GameWindowFlags.Default;
 
             if (Configuration.Fullscreen && !flags.HasFlag(GameWindowFlags.Fullscreen))
@@ -45,6 +50,48 @@ namespace Flamy2D
 
             if (!Configuration.Resizable && !flags.HasFlag(GameWindowFlags.Fullscreen))
                 flags |= GameWindowFlags.FixedWindow;
+
+            this.Log("Creating native window");
+            window = new NativeWindow(
+                width: Configuration.Width,
+                height: Configuration.Height,
+                title: Configuration.Title,
+                options: flags,
+                mode: GraphicsMode.Default,
+                device: DisplayDevice.Default
+            );
+
+            window.Icon = Flamy2D.Properties.Resources.Flamy;
+
+            updateResolution();
+
+            window.Resize += (o, e) => OnResize();
+
+        }
+
+        /// <summary>
+        /// Resizes the game. 
+        /// </summary>
+        /// <param name="w">The new width. </param>
+        /// <param name="h">The new height. </param>
+        public void Resize(int w, int h)
+        {
+            window.Width = w;
+            window.Height = h;
+
+            OnResize();
+        }
+
+        private void updateResolution()
+        {
+            Resolution = new Resolution(window.Width, window.Height);
+        }
+
+        protected virtual void OnResize()
+        {
+            updateResolution();
+
+
         }
 
 
---
+++ Flamy2D/Resolution.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Flamy2D
+{
+    /// <summary>
+    /// Contains the resolution information of the <see cref="GameEngine"/> 
+    /// </summary>
+    public class Resolution
+    {
+        /// <summary>
+        /// The width
+        /// </summary>
+        public int Width { get; set; }
+
+        /// <summary>
+        /// The height
+        /// </summary>
+        public int Height { get; set; }
+
+        /// <summary>
+        /// .ctor for <see cref="Resolution"/>
+        /// </summary>
+        /// <param name="w">The width of the window.</param>
+        /// <param name="h">The height of the window.</param>
+        public Resolution(int w, int h)
+        {
+            Width = w;
+            Height = h;
+        }
+
+        /// <summary>
+        /// Gets the aspect ratio of the game. 
+        /// </summary>
+        public float AspectRatio { get { return (float)Width / (float)Height; } }
+    }
+}
