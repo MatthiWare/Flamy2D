0632b03d19c689598ba6a7cd33d315ea699d9a16
Matthiee <beerens.matthias@hotmail.com>
2016-03-06 15:58:47 +0100

    Adding ability to set the actual image of an Texture2D

--- Flamy2D/Graphics/Texture2D.cs
+++ Flamy2D/Graphics/Texture2D.cs
@@ -22,65 +22,79 @@ namespace Flamy2D.Graphics
             TextureId = GL.GenTexture();
 
             // Bind the texture
-            Bind(TextureUnit.Texture0);
+            Bind();
+            { 
+                // Choose the filters
+                TextureMinFilter minFilter = TextureMinFilter.Linear;
+                TextureMagFilter magFilter = TextureMagFilter.Linear;
 
+                switch (config.Interpolation)
+                {
+                    case InterpolationMode.Linear:
+                        minFilter = config.Mipmap ? TextureMinFilter.LinearMipmapLinear : TextureMinFilter.Linear;
+                        break;
+                    case InterpolationMode.Nearest:
+                        minFilter = config.Mipmap ? TextureMinFilter.NearestMipmapNearest : TextureMinFilter.Nearest;
+                        magFilter = TextureMagFilter.Nearest;
+                        break;
+                }
 
-            // Choose the filters
-            TextureMinFilter minFilter = TextureMinFilter.Linear;
-            TextureMagFilter magFilter = TextureMagFilter.Linear;
+                // set the min filter parameter
+                GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)minFilter);
 
-            switch (config.Interpolation)
-            {
-                case InterpolationMode.Linear:
-                    minFilter = config.Mipmap ? TextureMinFilter.LinearMipmapLinear : TextureMinFilter.Linear;
-                    break;
-                case InterpolationMode.Nearest:
-                    minFilter = config.Mipmap ? TextureMinFilter.NearestMipmapNearest : TextureMinFilter.Nearest;
-                    magFilter = TextureMagFilter.Nearest;
-                    break;
+                // set the mag filter parameter
+                GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)magFilter);
+
+                // create the texture
+                GL.TexImage2D(
+                    target: TextureTarget.Texture2D,
+                    level: 0,
+                    internalformat: PixelInternalFormat.Rgba,
+                    width: Width,
+                    height: Height,
+                    border: 0,
+                    format: PixelFormat.Bgra,
+                    type: PixelType.UnsignedByte,
+                    pixels: IntPtr.Zero
+                );
+
+                if (config.Mipmap)
+                    GL.GenerateMipmap(GenerateMipmapTarget.Texture2D);
             }
+            Unbind();
+    }
 
-            // set the min filter parameter
-            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)minFilter);
+    public void SetData(IntPtr data, Rectangle? rect, PixelFormat format = PixelFormat.Rgba, PixelType type = PixelType.UnsignedByte)
+    {
+        Rectangle r = rect ?? Bounds;
 
-            // set the mag filter parameter
-            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)magFilter);
-
-            // create the texture
-            GL.TexImage2D(
+        Bind();
+        {
+            GL.TexSubImage2D(
                 target: TextureTarget.Texture2D,
                 level: 0,
-                internalformat: PixelInternalFormat.Rgba,
-                width: Width,
-                height: Height,
-                border: 0,
-                format: PixelFormat.Bgra,
-                type: PixelType.UnsignedByte,
-                pixels: IntPtr.Zero
+                xoffset: 0,
+                yoffset: 0,
+                width: r.Width,
+                height: r.Height,
+                format: format,
+                type: type,
+                pixels: data           
             );
-
-            if (config.Mipmap)
-                GL.GenerateMipmap(GenerateMipmapTarget.Texture2D);
-
-            Unbind(TextureUnit.Texture0);
-
         }
-
-        public void SetData(IntPtr data, Rectangle? rect, PixelFormat format = PixelFormat.Rgba, PixelType type = PixelType.UnsignedByte)
-        {
-
-        }
-
-        private void Unbind(TextureUnit texture0)
-        {
-            GL.ActiveTexture(texture0);
-            GL.BindTexture(TextureTarget.Texture2D, 0);
-        }
-
-        private void Bind(TextureUnit texture0)
-        {
-            GL.ActiveTexture(texture0);
-            GL.BindTexture(TextureTarget.Texture2D, TextureId);
-        }
+        Unbind();
     }
+
+    private void Unbind()
+    {
+        GL.ActiveTexture(TextureUnit.Texture0);
+        GL.BindTexture(TextureTarget.Texture2D, 0);
+    }
+
+    private void Bind()
+    {
+        GL.ActiveTexture(TextureUnit.Texture0);
+        GL.BindTexture(TextureTarget.Texture2D, TextureId);
+    }
+}
 }
