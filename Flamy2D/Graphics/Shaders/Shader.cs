3b48d1d7bb256e962de090dfb3dfe96d1317972f
Matthiee <beerens.matthias@hotmail.com>
2016-03-08 21:04:34 +0100

    Shader support

---
+++ Flamy2D/Graphics/Shaders/Shader.cs
@@ -0,0 +1,35 @@
+﻿using OpenTK.Graphics.OpenGL4;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Flamy2D.Graphics.Shaders
+{
+    public abstract class Shader
+    {
+        protected ShaderType shaderType;
+
+        protected string[] shaderSources;
+
+        protected int internalShaderID = -1;
+
+        public int ShaderId { get { return internalShaderID; } }
+
+        protected Shader(ShaderType type, params string[] sources)
+        {
+            shaderType = type;
+            shaderSources = sources;
+        }
+
+        public abstract Compile();
+
+        public virtual void Dispose()
+        {
+            if (internalShaderID != -1)
+                GL.DeleteShader(internalShaderID);
+
+            internalShaderID = -1;
+        }
+    }
+}
