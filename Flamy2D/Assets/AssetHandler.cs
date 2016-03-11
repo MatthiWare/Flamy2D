b6291708f8b9204d298ad56e04c9308b5e1d1e53
Matthiee <beerens.matthias@hotmail.com>
2016-03-11 19:17:26 +0100

    Asset classes done

--- Flamy2D/Assets/AssetHandler.cs
+++ Flamy2D/Assets/AssetHandler.cs
@@ -1,11 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.IO;
 
 namespace Flamy2D.Assets
 {
-    class AssetHandler
+    public abstract class AssetHandler<T> where T : IAsset
     {
+        public string AssetRoot { get; private set; }
+
+        public ContentManager Manager { get; private set; }
+
+        protected AssetHandler(ContentManager mgr, string root)
+        {
+            AssetRoot = root;
+            Manager = mgr;
+        }
+
+        public string GetAssetPath(string asset)
+        {
+            return Manager.NormalizePath(Path.Combine(Manager.ContentRoot, AssetRoot, asset));
+        }
+
+        public abstract T Load(string assetName, params object[] args);
+
+        public virtual void Save(T asset, string path)
+        { }
     }
 }
