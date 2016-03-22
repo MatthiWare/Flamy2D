8ee723333d3e2bea2b7faaa8d67adde807bb4e70
Matthiee <beerens.matthias@hotmail.com>
2016-03-22 17:02:01 +0100

    Added a non working sound provider

---
+++ Flamy2D/Assets/Providers/SoundProvider.cs
@@ -0,0 +1,20 @@
+﻿using Flamy2D.Audio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Flamy2D.Assets.Providers
+{
+    public class SoundProvider : AssetHandler<Sound>
+    {
+        public SoundProvider(ContentManager mgr)
+            : base(mgr, "sounds")
+        { }
+
+        public override Sound Load(string assetName, params object[] args)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
