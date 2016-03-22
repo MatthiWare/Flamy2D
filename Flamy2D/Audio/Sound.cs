1401d99ed3e84b67578b5d42f0c98ee755e29369
Matthiee <beerens.matthias@hotmail.com>
2016-03-22 17:00:37 +0100

    Added sound class

---
+++ Flamy2D/Audio/Sound.cs
@@ -0,0 +1,13 @@
+﻿using Flamy2D.Assets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Flamy2D.Audio
+{
+    public class Sound : IAsset
+    {
+
+    }
+}
