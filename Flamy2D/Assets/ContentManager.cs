be7ddfd9e9fa3960b66e9c3faa81a82d90a99bdf
Matthiee <beerens.matthias@hotmail.com>
2016-03-26 22:59:00 +0100

    Added dispose method for contentmanager

--- Flamy2D/Assets/ContentManager.cs
+++ Flamy2D/Assets/ContentManager.cs
@@ -5,7 +5,7 @@ using System.IO;
 
 namespace Flamy2D.Assets
 {
-    public class ContentManager : ILog
+    public class ContentManager : ILog, IDisposable
     {
 
         private string contentRoot;
@@ -84,6 +84,44 @@ namespace Flamy2D.Assets
                 provider.Save(asset, assetPath);
             }
         }
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    
+                }
+
+                AssetHandlers.Clear();
+                AssetHandlers = null;
+
+                // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
+                // TODO: set large fields to null.
+
+                disposedValue = true;
+            }
+        }
+
+        // TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
+        // ~ContentManager() {
+        //   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+        //   Dispose(false);
+        // }
+
+        // This code added to correctly implement the disposable pattern.
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(true);
+            // TODO: uncomment the following line if the finalizer is overridden above.
+            // GC.SuppressFinalize(this);
+        }
+        #endregion
 
     }
 }
